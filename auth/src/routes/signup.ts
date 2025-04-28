2114119c8776ade4650609b51641214d891b0b56
trcosner <trcosner@gmail.com>
2025-04-28 16:37:42 -0400

    feat: middleware for authorization and session check with jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/RequestValidationError";
+import { body } from "express-validator";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/BadRequestError";
 import jwt from "jsonwebtoken";
+import { ValidateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -16,13 +16,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
+  ValidateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
