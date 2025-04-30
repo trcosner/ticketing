afb949170ba1f3c76e79d17cf6f84c828a2a3fe3
trcosner <trcosner@gmail.com>
2025-04-30 18:49:42 -0400

    feat: react app auth flow

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -29,7 +29,6 @@ export const currentUser = (
       process.env.JWT_KEY!
     ) as UserPayload;
     req.currentUser = payload;
-    res.send({ currentUser: payload });
   } catch (err) {}
 
   next();
