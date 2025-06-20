c1fd7221ad13d26d05330f4ca705b2fd7ea7ee23
trcosner <trcosner@gmail.com>
2025-06-20 16:45:52 -0400

    fix: tests

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -16,7 +16,9 @@ it("clears the cookie after signing out", async () => {
   if (!cookie) {
     throw new Error("Expected cookie but got undefined.");
   }
-  expect(cookie[0]).toEqual(
-    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
-  );
+  
+  // Should clear both session and refreshToken cookies
+  expect(cookie).toHaveLength(2);
+  expect(cookie.some(c => c.includes("session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"))).toBe(true);
+  expect(cookie.some(c => c.includes("refreshToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT"))).toBe(true);
 });
